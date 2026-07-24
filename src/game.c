f381e7131cdf45965441de478c24c19d9a5cd65b
Rościsław Szymański <156237164+I-use-Arch-btw-00111@users.noreply.github.com>
2026-07-24 14:56:09 +0200

    Implement gameplay and UI rendering areas with proper layout

--- src/game.c
+++ src/game.c
@@ -1,6 +1,5 @@
 #include "game.h"
 #include "config.h"
-#include <raylib.h>
 
 static GameState g_game_state = {0};
 
@@ -17,13 +16,37 @@ void game_update(float dt) {
     // TODO: Game logic here
 }
 
-void game_render(void) {
-    // Debug text
-    DrawText("STFA - Touhou Fangame", 10, 10, 20, WHITE);
-    DrawText(TextFormat("Frame: %u", g_game_state.frame_count), 10, 35, 20, WHITE);
-    DrawText(TextFormat("Time: %.2f", g_game_state.time_elapsed), 10, 60, 20, WHITE);
+void game_render_gameplay(void) {
+    // Draw gameplay area border
+    DrawRectangleLines(GAMEPLAY_X, GAMEPLAY_Y, GAMEPLAY_WIDTH, GAMEPLAY_HEIGHT, DARKBLUE);
     
-    // TODO: Render game here
+    // Gameplay content
+    DrawText("Gameplay Area", GAMEPLAY_X + 10, GAMEPLAY_Y + 10, 16, BLUE);
+    
+    // TODO: Render game entities (player, enemies, bullets) here
+}
+
+void game_render_ui(void) {
+    // Draw UI area background
+    DrawRectangle(UI_X, UI_Y, UI_WIDTH, UI_HEIGHT, DARKGRAY);
+    
+    // Draw UI area border
+    DrawRectangleLines(UI_X, UI_Y, UI_WIDTH, UI_HEIGHT, GRAY);
+    
+    // UI Content
+    DrawText("UI Area", UI_X + 10, UI_Y + 10, 16, WHITE);
+    DrawText(TextFormat("Frame: %u", g_game_state.frame_count), UI_X + 10, UI_Y + 35, 14, LIGHTGRAY);
+    DrawText(TextFormat("Time: %.2f", g_game_state.time_elapsed), UI_X + 10, UI_Y + 55, 14, LIGHTGRAY);
+    
+    // TODO: Render UI elements (health, score, wave info) here
+}
+
+void game_render(void) {
+    // Render gameplay area (left)
+    game_render_gameplay();
+    
+    // Render UI area (right)
+    game_render_ui();
 }
 
 void game_cleanup(void) {
